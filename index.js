afa0cb0259296ea4592f9bc93e5edf39a7460dc7
Priscila Rodriguez Fresco <priscila.rodriguezfresco@gmail.com>
2024-08-27 13:08:48 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+// Define num1 and num2
+let num1 = 31;
+let num2 = 2;
+// Equation whose multiplied results will equal 62
+let multiply = num1 * num2;
+
+// Generates random integer greater than 0
+var random = Math.floor(Math.random() * 10) + 1;
+
+// An equation that calculates as a remainder that is equal to 4
+let num3 = 12
+let num4 = 3
+
+var mod = num3 / num4
+
+// A variable that finds the highest number in a set
+var max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
